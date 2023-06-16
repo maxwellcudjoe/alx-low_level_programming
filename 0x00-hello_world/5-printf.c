f4819493b40ae0772f2625c9d28545e1d11afe65
maxwellcudjoe <cudjoemaxwell@gmail.com>
2023-06-15 22:40:24 -0700

    very diffic

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("With proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
