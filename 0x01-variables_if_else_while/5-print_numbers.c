efee2876cbd1018615a049bfb5671290f66efa59
maxwellcudjoe <cudjoemaxwell@gmail.com>
2023-06-19 23:00:20 -0700

    upd

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - prints numbers from 0 to 9.
  * 
@@ -8,12 +7,13 @@
 */
 int main(void)
 {
-    
     int i;
+
     for (i = 0; i < 10; i++)
     {
-      printf("%d",i);
+        printf("%d", i);
     }
+
     putchar('\n');
     return (0);
 }
